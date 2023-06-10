df5590cf8f6ef1c2a4491a873b67811d49aec358
udop <green3623@gmail.com>
2023-06-10 16:27:46 +0900

    旧拡張の内容を適用

---
+++ content.ts
@@ -0,0 +1,56 @@
+import type { PlasmoCSConfig } from "plasmo"
+import { Storage } from "@plasmohq/storage"
+
+const storage = new Storage()
+
+let muteList: string[] = []
+storage.watch({
+    "userList": (c) => {
+        muteList = c.newValue
+    }
+})
+
+
+export const config: PlasmoCSConfig = {
+    matches: ["https://asobistage.asobistore.jp/*"],
+    all_frames: true
+}
+
+
+window.addEventListener("load", () => {
+
+    setTimeout(() => {
+
+
+
+        const target = document.querySelector("div[class^='commentViewer_commentList__'] > div:nth-child(2) > div > div")
+        if (!target) {
+            return
+        }
+        const observer = new MutationObserver(function (mutations) {
+            for (var m of mutations) {
+                for (var comment of m.addedNodes) {
+                    const element = comment as any
+                    var name = element.querySelector("*[class^='commentViewer_item_nickName_link']")
+                    var text = element.querySelector("*[class^='commentViewer_item_comment']")
+                    if (!name || !text) {
+                        console.log(name, text)
+                        continue
+                    }
+
+                    if (muteList.includes(name.innerText)) {
+                        name.innerText = ""
+                        text.innerHTML = "<i style='color: #666;'>muted</i>"
+                    }
+                }
+            }
+        });
+
+        observer.observe(target, { attributes: true, childList: true })
+
+    }, 3000);
+
+
+})
+
+export { }
--- popup.vue
+++ popup.vue
@@ -1,8 +1,63 @@
 <script lang="ts" setup>
-import { ref } from "vue"
-const name = ref("vue")
+import { onMounted, ref } from "vue"
+import { Storage } from "@plasmohq/storage"
+
+const storage = new Storage()
+
+const input = ref("")
+const userList = ref<string[]>([])
+
+const add = () => {
+    if (input.value == "") {
+        return
+    }
+
+    userList.value.push(input.value)
+    input.value = ""
+    update()
+}
+
+const remove = (user: string) => {
+    userList.value = userList.value.filter(x => x != user)
+    update()
+}
+
+const update = async () => {
+    console.log("update!")
+    await storage.set("userList", userList.value)
+}
+
+onMounted(async () => {
+    const list = await storage.get<string[]>("userList") ?? []
+
+    if (Array.isArray(list)) {
+        userList.value = list
+    } else {
+        console.warn("err: userList is not array!", list)
+        userList.value = []
+    }
+})
+
 </script>
 
 <template>
-    <div>Hello, {{ name }}!</div>
+    <div class="container">
+        <h2>ASOBISTAGE コメントミュート</h2>
+        <div>ミュートしたい名前を追加してください（完全一致）</div>
+        <input type="text" v-model="input" @keydown.enter="add">
+        <input type="button" value="追加" @click="add">
+        <table id="muteList">
+            <tr v-for="user in userList">
+                <td>{{ user }}</td>
+                <td><button type="button" @click="remove(user)">削除</button></td>
+            </tr>
+        </table>
+    </div>
 </template>
+
+<style scoped>
+.container {
+    width: 360px;
+    height: 480px;
+}
+</style>
